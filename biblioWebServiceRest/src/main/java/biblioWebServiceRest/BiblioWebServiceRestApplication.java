9ed6b30a90ccf7bfa82fa36ae51a738f76dfc40d
Jean-Louis PIERA <jeanlouispiera@yahoo.fr>
2020-08-15 17:00:24 +0200

    Refactor du datasource de l'API
    Application.class et application.properties

--- biblioWebServiceRest/src/main/java/biblioWebServiceRest/BiblioWebServiceRestApplication.java
+++ biblioWebServiceRest/src/main/java/biblioWebServiceRest/BiblioWebServiceRestApplication.java
@@ -1,31 +1,11 @@
 package biblioWebServiceRest;
 
-
-
-
-
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.init.DatabasePopulator;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-
-
-
-
 
 
 @SpringBootApplication
-(exclude=DataSourceAutoConfiguration.class)
 @EnableWebSecurity
 public class BiblioWebServiceRestApplication  {
 	
@@ -33,24 +13,6 @@ public class BiblioWebServiceRestApplication  {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BiblioWebServiceRestApplication.class, args);
-	}
-	
-	@Bean(name = "dataSource")
-	public DriverManagerDataSource dataSource() {
-		//initialisation du driver Postgresql
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName("org.postgresql.Driver");
-	    dataSource.setUrl("jdbc:postgresql://localhost:5432/biblio");
-	    dataSource.setUsername("postgres");
-	    dataSource.setPassword("postgres");
-
-	    // initialiation de la base
-	    Resource initSchema = new ClassPathResource("schema.sql");
-	    Resource initData = new ClassPathResource("data.sql");
-	    DatabasePopulator databasePopulator = new ResourceDatabasePopulator(initSchema, initData);
-	    DatabasePopulatorUtils.execute(databasePopulator, dataSource);
-
-	    return dataSource;
 	}
 	
 	
